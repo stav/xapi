79045fe2774aca92653ab724d8d56d240504ed96
Steven Almeroth <sroth77@gmail.com>
2021-10-08 15:40:56 -0400

    update

--- main.ts
+++ main.ts
@@ -2,17 +2,17 @@ import fs from 'fs'
 import XAPI from 'xapi-node'
 import { CMD_FIELD, TYPE_FIELD } from 'xapi-node'
 
-async function buyGold(xapi: XAPI) {
+async function buySellGold(xapi: XAPI) {
   return await xapi.Socket.send.tradeTransaction({
-    cmd: CMD_FIELD.BUY_LIMIT,
+    cmd: CMD_FIELD.SELL_LIMIT,
     customComment: null,
     expiration: new Date().getTime() + 60000 * 60 * 24 * 365,
     offset: 0,
     order: 0,
-    price: 1752,
-    sl: 1750,
+    price: 1765,
+    sl: 1775,
     symbol: 'GOLD',
-    tp: 1756,
+    tp: 1763,
     type: TYPE_FIELD.OPEN,
     volume: 10,
   })
@@ -25,11 +25,20 @@ async function writeAllSymbols(xapi: XAPI) {
   return `${result.returnData.length} symbols written`
 }
 
+async function updateStoploss(xapi: XAPI) {
+  return await xapi.Socket.send.tradeTransaction({
+    order: xapi.positions[0].position,
+    type: TYPE_FIELD.MODIFY,
+    sl: 1754,
+  })
+}
+
 export default async function (xapi: XAPI) {
 
   console.log('main')
-  // const result = await buyGold(xapi)
-  const result = await writeAllSymbols(xapi)
+  // const result = await buySellGold(xapi)
+  // const result = await writeAllSymbols(xapi)
+  const result = await updateStoploss(xapi)
   console.log('Result', result)
 
 }
