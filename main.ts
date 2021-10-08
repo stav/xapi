ed889e952e1f70ed91c1a02dc832308e2f5f8f4c
Steven Almeroth <sroth77@gmail.com>
2021-10-07 20:12:26 -0400

    gold

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
 import XAPI from 'xapi-node'
 import { CMD_FIELD, TYPE_FIELD } from 'xapi-node'
 
-export default async function (xapi: XAPI) {
-
-  console.log('main')
-
-  const transaction = {
+async function buyGold(xapi: XAPI) {
+  return await xapi.Socket.send.tradeTransaction({
     cmd: CMD_FIELD.BUY_LIMIT,
     customComment: null,
     expiration: new Date().getTime() + 60000 * 60 * 24 * 365,
     offset: 0,
     order: 0,
-    price: 100,
-    sl: 0,
-    symbol: 'BITCOIN',
-    tp: 1000,
+    price: 1752,
+    sl: 1750,
+    symbol: 'GOLD',
+    tp: 1756,
     type: TYPE_FIELD.OPEN,
-    volume: 10
-  }
-  const result = await xapi.Socket.send.tradeTransaction(transaction)
+    volume: 10,
+  })
+}
+
+export default async function (xapi: XAPI) {
+
+  console.log('main')
+  const result = await buyGold(xapi)
   console.log('Result', result)
 
 }
