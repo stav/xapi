e7c61786e9253f80f9c3b3021c0b851cefb4ab14
Steven Almeroth <sroth77@gmail.com>
2021-10-13 09:06:39 -0400

    map api calls

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,21 @@ import fs from 'fs'
 import config from 'config'
 
 import XAPI from 'xapi-node'
-import { CMD_FIELD, TYPE_FIELD } from 'xapi-node'
+import {
+  CMD_FIELD,
+  TYPE_FIELD,
+  ConnectionStatus,
+  STREAMING_TRADE_RECORD as streamingTradeRecord,
+  STREAMING_TRADE_STATUS_RECORD as streamingTradeStatusRecord,
+} from 'xapi-node'
+
+function getTrades (data: streamingTradeRecord) {
+  console.log('getTrades', data)
+}
+
+function getTradeStatus (data: streamingTradeStatusRecord) {
+  console.log('getTradeStatus', data)
+}
 
 const orders = function () {
   const tip: any = config.get('Tip.XAUUSD')
@@ -20,6 +34,13 @@ const orders = function () {
     volume: 10,
   }))
 }()
+
+export async function disconnect (xapi: XAPI) {
+  process.stdout.write('disconnecting... ')
+  await xapi.disconnect()
+  process.stdout.write(`${ConnectionStatus[xapi.Socket.status]}\n`)
+  process.exit();
+}
 
 export async function buySellGold(xapi: XAPI) {
   for (const order of orders) {
@@ -40,4 +61,20 @@ export async function updateStoploss(xapi: XAPI) {
     type: TYPE_FIELD.MODIFY,
     sl: 1754,
   })
+}
+
+export async function listenForTrades(xapi: XAPI) {
+  // xapi.Stream.subscribe.getBalance().catch(console.error)
+  xapi.Stream.listen.getTrades(getTrades)
+  xapi.Stream.listen.getTradeStatus(getTradeStatus)
+  xapi.Stream.subscribe.getTrades().catch(console.error)
+  xapi.Stream.subscribe.getTradeStatus().catch(console.error)
+}
+
+export async function unListenForTrades(xapi: XAPI) {
+  console.log('UNListing')
+  // xapi.Stream.unSubscribe.getBalance().catch(console.error)
+  // xapi.Stream.subscribe.getTickPrices('EURUSD').catch(() => { console.error('subscribe for EURUSD failed')})
+  xapi.Stream.unSubscribe.getTrades().catch(console.error)
+  xapi.Stream.unSubscribe.getTradeStatus().catch(console.error)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,35 @@
 import XAPI from 'xapi-node'
 import { ConnectionStatus } from 'xapi-node'
-import { STREAMING_TRADE_RECORD as streamingTradeRecord } from 'xapi-node'
-import { STREAMING_TRADE_STATUS_RECORD as streamingTradeStatusRecord } from 'xapi-node'
 
-import { buySellGold, updateStoploss, writeAllSymbols } from './api'
+import {
+  disconnect,
+  buySellGold,
+  updateStoploss,
+  writeAllSymbols,
+  listenForTrades,
+  unListenForTrades,
+} from './api'
 
 const ctrlC = '\u0003'
 const ctrlD = '\u0004'
 
-function getTrades (data: streamingTradeRecord) {
-  console.log('getTrades', data)
-}
-
-function getTradeStatus (data: streamingTradeStatusRecord) {
-  console.log('getTradeStatus', data)
+type KeyMap = {
+  "\u0003": (xapi: XAPI) => Promise<void>
+  "\u0004": (xapi: XAPI) => Promise<void>
 }
 
 export default async function (xapi: XAPI) {
   console.log('Socket is:', ConnectionStatus[xapi.Socket.status])
+
+  const keyMap = {
+    [ctrlC]: disconnect,
+    [ctrlD]: disconnect,
+    1: listenForTrades,
+    2: unListenForTrades,
+    3: buySellGold,
+    4: writeAllSymbols,
+    5: updateStoploss,
+  }
 
   const stdin = process.stdin
   stdin.setEncoding('utf8')
@@ -29,38 +41,8 @@ export default async function (xapi: XAPI) {
     process.stdout.write(JSON.stringify(data) + ' ')
     const key = data.toString()
 
-    if ( key === ctrlC || key === ctrlD ) {
-      process.stdout.write('disconnecting... ')
-      await xapi.disconnect()
-      process.stdout.write(`${ConnectionStatus[xapi.Socket.status]}\n`)
-      process.exit();
-    }
-    else if ( key === '1' ) {
-      console.log('Trades')
-      // xapi.Stream.subscribe.getBalance().catch(console.error)
-      xapi.Stream.listen.getTrades(getTrades)
-      xapi.Stream.listen.getTradeStatus(getTradeStatus)
-      xapi.Stream.subscribe.getTrades().catch(console.error)
-      xapi.Stream.subscribe.getTradeStatus().catch(console.error)
-    }
-    else if ( key === '2' ) {
-      console.log('UNListing')
-      // xapi.Stream.unSubscribe.getBalance().catch(console.error)
-      // xapi.Stream.subscribe.getTickPrices('EURUSD').catch(() => { console.error('subscribe for EURUSD failed')})
-      xapi.Stream.unSubscribe.getTrades().catch(console.error)
-      xapi.Stream.unSubscribe.getTradeStatus().catch(console.error)
-    }
-    else if ( key === '3' ) {
-      console.log('Buy or sell gold')
-      await buySellGold(xapi)
-    }
-    else if ( key === '4' ) {
-      console.log('Write all symbols')
-      await writeAllSymbols(xapi)
-    }
-    else if ( key === '5' ) {
-      console.log('Update stop loss')
-      await updateStoploss(xapi)
+    if ( key in keyMap ) {
+      await keyMap[key as keyof KeyMap](xapi)
     }
     else {
       process.stdout.write('does nothing\n')
