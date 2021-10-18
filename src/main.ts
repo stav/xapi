0c303632c69e004b1c87e532920c07b1f5078c94
Steven Almeroth <sroth77@gmail.com>
2021-10-18 09:06:54 -0400

    stdin module

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,10 @@
 import XAPI from 'xapi-node'
-import keyMap from './keyMap'
-import { KeyMap } from './keyMap'
-import { setupStdin, socketStatus } from './utils'
-
-const carriage = ['\n', '\r', '\r\n']
+import stdIn from './stdIn'
+import { setup as setupStdin } from './stdIn'
+import { socketStatus } from './utils'
 
 export default function (xapi: XAPI) {
   console.log('Socket is:', socketStatus(xapi))
-
   setupStdin()
-
-  process.stdin.on('data', stdIn)
-
-  async function stdIn (data: Buffer) {
-    process.stdout.write('\n' + JSON.stringify(data) + ' ')
-
-    const key = data.toString() as keyof KeyMap
-
-    // If the key the user pressed is in the map, it's a function, execute it
-    if (key in keyMap) {
-      // xapi is being hoisted from the outer function
-      await keyMap[ key ]( xapi )
-    }
-    // Else if the key is the enter key ignore it
-    else if (carriage.includes(key)) {
-      process.stdout.write('\n')
-    }
-    // Otherwise show the list of valid keys
-    else {
-      process.stdout.write('does nothing\n')
-      for (const _ in keyMap) {
-        process.stdout.write(JSON.stringify(_) + ' ')
-        const func = keyMap[_ as keyof KeyMap]
-        process.stdout.write(func.name + '\n')
-      }
-    }
-  }
+  process.stdin.on('data', (data) => stdIn(data, xapi))
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,3 @@ import XAPI from 'xapi-node'
 import { ConnectionStatus } from 'xapi-node'
 
 export const socketStatus = (xapi: XAPI) => ConnectionStatus[xapi.Socket.status]
-
-export function setupStdin () {
-  process.stdin.setEncoding('utf8')
-  process.stdin.setRawMode(true) // false sends chunk after enter is pressed
-  process.stdin.resume() // running in parent process event loop
-}
