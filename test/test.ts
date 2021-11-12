bb5d2005d7424327be5507cf6f75550f50643e95
Steven Almeroth <sroth77@gmail.com>
2021-11-12 09:19:23 -0500

    use test dir

---
+++ test/test.ts
@@ -0,0 +1,21 @@
+describe("WebSocket Server", () => {
+  beforeAll(() => {
+    // Start server
+  });
+
+  afterAll(() => {
+    // Close server
+  });
+
+  test("Server echoes the message it receives from client", () => {
+    // 1. Create test client
+    // 2. Send client message
+    // 3. Close the client after it receives the response
+    // 4. Perform assertions on the response
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+});
